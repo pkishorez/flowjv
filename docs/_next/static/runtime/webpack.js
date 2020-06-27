bfee095c960c47436864b48986ed3471508c0c48
Kishore <kishore.iiitn@gmail.com>
2020-06-27 21:39:25 +0530

    PUBLISH DOCS!

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e5d251589d0437f1a873";
+/******/ 	var hotCurrentHash = "1b13e5cab6401e234215";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

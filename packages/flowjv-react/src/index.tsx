537a388b456a0ca565f5a25de453e4c54a0edf4b
Kishore <kishore.iiitn@gmail.com>
2020-07-05 22:10:03 +0530

    fix: export flowjv functions and types from flowjv-react

--- packages/flowjv-react/src/index.tsx
+++ packages/flowjv-react/src/index.tsx
@@ -2,4 +2,4 @@ export { setupFlowJV, FlowJVForm } from "./bindings/form/index";
 export { Field } from "./bindings/form/custom/field";
 export { IfBlock, Block } from "./bindings/form/custom/block";
 
-export { defaultConfig } from "./bindings/form/config/default";
+export { validateJSONFlow, IFlowSchema } from "flowjv";

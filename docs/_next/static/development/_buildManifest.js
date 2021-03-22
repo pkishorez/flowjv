50432d10550c67b08e13cfb93f0bfffedfe92e26
Kishore <kishore.iiitn@gmail.com>
2021-03-22 22:59:33 +0530

    PUBLISH DOCS!

---
+++ docs/_next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/playground":["static\u002Fchunks\u002Fpages\u002Fplayground.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fplayground"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
